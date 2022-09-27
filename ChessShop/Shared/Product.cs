8b44fd4635d5f2271c7858ff0972fe2b3842f984
Ion Mladinoi <ion.mladinoi@levi9.com>
2022-09-27 17:34:32 +0300

    created the product list component

---
+++ ChessShop/Shared/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ChessShop.Shared
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string Image { get; set; } = "https://www.pexels.com/photo/black-fujifilm-dslr-camera-90946/";
+        public decimal Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public bool IsPublic { get; set; }
+        public bool IsDeleted { get; set; }
+        public Category Category { get; set; } = new();
+        public int CategoryId { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateUpdated { get; set; }
+    }
+}
